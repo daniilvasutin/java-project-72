18f392173196fb155d48fe07f4118560e7fb2eb6
daniilvasutin <daniilvasutin@mail.ru>
2023-11-03 16:39:03 +0300

    2st doker try

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,9 @@
 package hexlet.code;
 
-public class Main {
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
