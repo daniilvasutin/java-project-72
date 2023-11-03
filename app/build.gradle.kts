76d7f1ebcfe959ed7c99c05b9b27cc09de3aa286
daniilvasutin <daniilvasutin@mail.ru>
2023-11-03 13:15:56 +0300

    1st start project 4

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+}
+
+application {
+    mainClass.set("hexlet.code.Main")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+tasks.withType<Checkstyle>().configureEach {
+    reports {
+        xml.required.set(false)
+        html.required.set(true)
+    }
+}
