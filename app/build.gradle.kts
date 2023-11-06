2714254f10120c2032a3bc3dd7b5bff742bb92f0
daniilvasutin <daniilvasutin@mail.ru>
2023-11-06 12:28:26 +0300

    3st 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.5")
     implementation("org.projectlombok:lombok:1.18.26")
     implementation("org.projectlombok:lombok:1.18.26")
+    implementation("org.postgresql:postgresql:42.1.4")
 
     implementation("com.zaxxer:HikariCP:5.1.0")
     implementation("com.h2database:h2:2.2.220")
